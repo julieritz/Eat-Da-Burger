2bb42744d1a682f0dd5907ea5248d1e4fa778483
julieritz <julie.ritz@gmail.com>
2020-06-01 11:53:23 -0700

    Adding connection, db and html

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: "burger_db",
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("BLT Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cali Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Straight-up Cheese Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Foraged Mushroom Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Asian Zing Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Country Fried Chicken Burger", true);
